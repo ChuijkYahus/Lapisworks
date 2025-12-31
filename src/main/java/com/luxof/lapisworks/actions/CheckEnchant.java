27092f87367249863e73e6e67b6c0683ca1afe4f
Luxof <luxtheluxof@gmail.com>
2025-12-31 20:42:20 +0500

    check enchant arguments were flipped

--- src/main/java/com/luxof/lapisworks/actions/CheckEnchant.java
+++ src/main/java/com/luxof/lapisworks/actions/CheckEnchant.java
@@ -1,54 +1,29 @@
 package com.luxof.lapisworks.actions;
 
-import java.util.List;
-
-import at.petrak.hexcasting.api.casting.OperatorUtils;
-import at.petrak.hexcasting.api.casting.castables.ConstMediaAction;
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
-import at.petrak.hexcasting.api.casting.eval.OperationResult;
-import at.petrak.hexcasting.api.casting.eval.vm.CastingImage;
-import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation;
 import at.petrak.hexcasting.api.casting.iota.DoubleIota;
 import at.petrak.hexcasting.api.casting.iota.Iota;
 import at.petrak.hexcasting.api.misc.MediaConstants;
 
 import com.luxof.lapisworks.mixinsupport.LapisworksInterface;
+import com.luxof.lapisworks.nocarpaltunnel.ConstMediaActionNCT;
+import com.luxof.lapisworks.nocarpaltunnel.HexIotaStack;
 
-public class CheckEnchant implements ConstMediaAction {
+import java.util.List;
+
+public class CheckEnchant extends ConstMediaActionNCT {
+    public int argc = 2;
+    public long mediaCost = (long)(MediaConstants.DUST_UNIT * 0.01);
+
     @Override
-    public List<Iota> execute(List<? extends Iota> args, CastingEnvironment ctx) {
-        LapisworksInterface ent = (LapisworksInterface)OperatorUtils.getLivingEntityButNotArmorStand(
-            args,
-            1,
-            getArgc()
-        );
+    public List<Iota> execute(HexIotaStack args, CastingEnvironment ctx) {
+        LapisworksInterface ent = (LapisworksInterface)args.getLivingEntityButNotArmorStand(0);
         return List.of(
             new DoubleIota(
                 (ent).getEnchantments().get(
-                    OperatorUtils.getIntBetween(args, 0, 0, ent.getEnchantments().size() - 1, getArgc())
+                    args.getIntBetween(1, 0, ent.getEnchantments().size() - 1)
                 )
             )
         );
-    }
-
-    @Override
-    public CostMediaActionResult executeWithOpCount(List<? extends Iota> arg0, CastingEnvironment arg1) {
-        return ConstMediaAction.DefaultImpls.executeWithOpCount(this, arg0, arg1);
-    }
-
-    @Override
-    public int getArgc() {
-        return 2;
-    }
-
-    @Override
-    public long getMediaCost() {
-        return (long)(MediaConstants.DUST_UNIT * 0.01);
-    }
-
-    @Override
-    public OperationResult operate(CastingEnvironment arg0, CastingImage arg1, SpellContinuation arg2) {
-        return ConstMediaAction.DefaultImpls.operate(this, arg0, arg1, arg2);
-    }
-    
+    }    
 }
