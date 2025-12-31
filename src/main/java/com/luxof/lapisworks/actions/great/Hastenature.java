69977f0afe42e8d3bcc3fd5844f7118eba6bd741
Luxof <luxtheluxof@gmail.com>
2025-12-31 20:34:54 +0500

    Fixed Hastenature being fucked

--- src/main/java/com/luxof/lapisworks/actions/great/Hastenature.java
+++ src/main/java/com/luxof/lapisworks/actions/great/Hastenature.java
@@ -1,98 +1,63 @@
 package com.luxof.lapisworks.actions.great;
 
-import at.petrak.hexcasting.api.casting.OperatorUtils;
 import at.petrak.hexcasting.api.casting.ParticleSpray;
-import at.petrak.hexcasting.api.casting.RenderedSpell;
 import at.petrak.hexcasting.api.casting.castables.SpellAction;
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
-import at.petrak.hexcasting.api.casting.eval.OperationResult;
-import at.petrak.hexcasting.api.casting.eval.vm.CastingImage;
-import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation;
-import at.petrak.hexcasting.api.casting.iota.Iota;
 import at.petrak.hexcasting.api.casting.mishaps.MishapUnenlightened;
 import at.petrak.hexcasting.api.misc.MediaConstants;
 
 import java.util.List;
 
-import org.jetbrains.annotations.Nullable;
-
-import com.luxof.lapisworks.MishapThrowerJava;
+import com.luxof.lapisworks.nocarpaltunnel.HexIotaStack;
+import com.luxof.lapisworks.nocarpaltunnel.SpellActionNCT;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 
 // name from chatgpt, sorry but i'm bad at naming things
 // and Hastenature is a name that kicks ass you have to admit
-public class Hastenature implements SpellAction {
-    public int getArgc() {
-        return 2;
-    }
+public class Hastenature extends SpellActionNCT {
+    public int argc = 2;
 
     @Override
-    public SpellAction.Result execute(List<? extends Iota> args, CastingEnvironment ctx) {
-        if (!ctx.isEnlightened()) { MishapThrowerJava.throwMishap(new MishapUnenlightened()); }
-        BlockPos pos = OperatorUtils.getBlockPos(args, 0, getArgc());
-        int times = OperatorUtils.getPositiveInt(args, 1, getArgc());
+    public Result execute(HexIotaStack stack, CastingEnvironment ctx) {
+        if (!ctx.isEnlightened()) { throw new MishapUnenlightened(); }
+        BlockPos pos = stack.getBlockPos(0);
+        int times = stack.getPositiveInt(1);
+
         BlockState state = ctx.getWorld().getBlockState(pos);
         int buddingAmethystPenalty = state.isOf(Blocks.BUDDING_AMETHYST) ? 3 : 1;
 
         return new SpellAction.Result(
-            new Spell(state, pos, times),
+            new Spell(pos, times),
             MediaConstants.SHARD_UNIT * buddingAmethystPenalty * times,
             List.of(ParticleSpray.burst(ctx.mishapSprayPos(), 2, 15)),
             times
         );
     }
 
-    public class Spell implements RenderedSpell {
-        public final BlockState state;
+    public class Spell implements RenderedSpellNCT {
         public final BlockPos pos;
         public final int times;
 
-        public Spell(BlockState state, BlockPos pos, int times) {
-            this.state = state;
+        public Spell(BlockPos pos, int times) {
             this.pos = pos;
             this.times = times;
         }
 
 		@Override
 		public void cast(CastingEnvironment ctx) {
+            ServerWorld world = ctx.getWorld();
+
             for (int i = 0; i < times; i++) {
-                this.state.randomTick(ctx.getWorld(), this.pos, ctx.getWorld().random);
+                world.getBlockState(pos).randomTick(
+                    world,
+                    pos,
+                    world.random
+                );
             }
 		}
-
-        @Override
-        public CastingImage cast(CastingEnvironment arg0, CastingImage arg1) {
-            return RenderedSpell.DefaultImpls.cast(this, arg0, arg1);
-        }
-    }
-
-    @Override
-    public boolean awardsCastingStat(CastingEnvironment ctx) {
-        return SpellAction.DefaultImpls.awardsCastingStat(this, ctx);
-    }
-
-    @Override
-    public Result executeWithUserdata(List<? extends Iota> args, CastingEnvironment env, NbtCompound userData) {
-        return SpellAction.DefaultImpls.executeWithUserdata(this, args, env, userData);
-    }
-
-    @Override
-    public boolean hasCastingSound(CastingEnvironment ctx) {
-        return SpellAction.DefaultImpls.hasCastingSound(this, ctx);
-    }
-
-    @Override
-    public OperationResult operate(CastingEnvironment arg0, CastingImage arg1, SpellContinuation arg2) {
-        return SpellAction.DefaultImpls.operate(this, arg0, arg1, arg2);
-    }
-
-    @Nullable
-    public static ServerPlayerEntity getPlayerOrNull(CastingEnvironment ctx) {
-        return ctx.getCastingEntity() != null ? (ServerPlayerEntity)ctx.getCastingEntity() : null;
     }
 }
