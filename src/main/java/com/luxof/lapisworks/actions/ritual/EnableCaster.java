e6f6ac4c4ca34415297f1e67f3ce9953bd42b715
Luxof <luxtheluxof@gmail.com>
2026-01-29 15:13:14 +0500

    don't make those spells into ConstMedias dumbass

--- src/main/java/com/luxof/lapisworks/actions/ritual/EnableCaster.java
+++ src/main/java/com/luxof/lapisworks/actions/ritual/EnableCaster.java
@@ -1,22 +1,36 @@
 package com.luxof.lapisworks.actions.ritual;
 
+import at.petrak.hexcasting.api.casting.ParticleSpray;
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
-import at.petrak.hexcasting.api.casting.iota.Iota;
 
-import com.luxof.lapisworks.nocarpaltunnel.ConstMediaActionNCT;
+import com.luxof.lapisworks.chalk.OneTimeRitualExecutionState;
 import com.luxof.lapisworks.nocarpaltunnel.HexIotaStack;
+import com.luxof.lapisworks.nocarpaltunnel.SpellActionNCT;
 
 import static com.luxof.lapisworks.MishapThrowerJava.assertInOneTimeRitual;
 
 import java.util.List;
 
-public class EnableCaster extends ConstMediaActionNCT {
+public class EnableCaster extends SpellActionNCT {
     public int argc = 0;
-    public long mediaCost = 0;
 
     @Override
-    public List<? extends Iota> execute(HexIotaStack stack, CastingEnvironment env) {
-        assertInOneTimeRitual(env).enableCaster();
-        return List.of();
+    public Result execute(HexIotaStack stack, CastingEnvironment env) {
+        return new Result(
+            new Spell(assertInOneTimeRitual(env)),
+            0L,
+            List.of(ParticleSpray.burst(ctx.mishapSprayPos(), 1, 5)),
+            1
+        );
+    }
+
+    public class Spell implements RenderedSpellNCT {
+        public final OneTimeRitualExecutionState ritual;
+        public Spell(OneTimeRitualExecutionState ritual) {
+            this.ritual = ritual;
+        }
+        public void execute(CastingEnvironment env) {
+            ritual.enableCaster();
+        }
     }
 }
