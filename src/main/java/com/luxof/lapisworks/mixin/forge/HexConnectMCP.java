d6b3d38185a1f6a75fe03831faa5c56feec8b3a1
Real-Luxof <luxtheluxof@gmail.com>
2026-04-09 05:29:11 +0500

    hopefully unflaying fixed on forge

---
+++ src/main/java/com/luxof/lapisworks/mixin/forge/HexConnectMCP.java
@@ -0,0 +1,11 @@
+package com.luxof.lapisworks.mixin.forge;
+
+import com.luxof.lapisworks.mixin.plugins.ModSpecificMCP;
+
+public class HexConnectMCP extends ModSpecificMCP {
+
+    protected HexConnectMCP() {
+        super("hexconnect", null);
+    }
+    
+}
